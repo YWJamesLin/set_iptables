8ebabad9119bacf7ed3e99498d7886552fa29c0d
YWJamesLin <JamesLin@ssinrc.org>
2015-09-16 22:39:47 +0800

    Updated

--- setNipt.sh
+++ setNipt.sh
@@ -63,12 +63,12 @@ done
 #iptables -A INPUT -i ${OutIF} -p TCP --dport 110 --sport 1024:65534 -j ACCEPT
 
 #   SAMBA
-#iptables -A INPUT -i ${OutIF} -p UDP --dport 137:138 --sport 1024:65534 -j ACCEPT
-#iptables -A INPUT -i ${OutIF} -p TCP --dport 139:445 --sport 1024:65534 -j ACCEPT
+#iptables -A INPUT -i ${OutIF} -p TCP --dport 139,445 --sport 1024:65534 -j ACCEPT
+#iptables -A INPUT -i ${OutIF} -p UDP --dport 137,138 --sport 1024:65534 -j ACCEPT
 
 #   NFS
 #iptables -A INPUT -i ${OUTIF} -p TCP --dport 111 --sport 1024:65534 -j ACCEPT
-#iptables -A INPUT -i ${OUTIF} -p UDP --dport 111:893 --sport 1024:65534 -j ACCEPT
+#iptables -A INPUT -i ${OUTIF} -p UDP --dport 111,893 --sport 1024:65534 -j ACCEPT
 
 #   HTTPS
 #iptables -A INPUT -i ${OutIF} -p TCP --dport 443 --sport 1024:65534 -j ACCEPT
@@ -76,8 +76,7 @@ done
 #   IPSec VPN
 #iptables -A INPUT -i ${OutIF} -p esp -j ACCEPT
 #iptables -A INPUT -i ${OutIF} -p ah -j ACCEPT
-#iptables -A INPUT -i ${OutIF} -p udp --dport 500 --sport 1024:65534 -j ACCEPT
-#iptables -A INPUT -i ${OutIF} -p udp --dport 4500 --sport 1024:65534 -j ACCEPT
+#iptables -A INPUT -i ${OutIF} -p udp --dport 500,4500 --sport 1024:65534 -j ACCEPT
 #iptables -t nat -A POSTROUTING -o ${OutIF} -s 10.1.0.0/16 -j MASQUERADE
 
 # Save Configuration
